706c3ccdd881a735bfbc36bc9d9e49657e413d94
Jacky <jackypengsmail@gmail.com>
2022-11-16 20:12:48 -0500

    Implemented functionality to fetch and display pokemon traits in container

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ import axios from "axios"
 
 //npm create vite@latest pokedex-web -- --template vanilla
 
+function firstUpperCase(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
 async function fetchAllPokemon() {
   try {
@@ -71,20 +74,84 @@ async function searchPokemon() {
   pokeSearchBtn.addEventListener("click", async () => {
     const pokeData = await fetchPokemon(pokeSearchBar.value);
     // console.log(pokeData.sprites.front_default)
-    pokemonName.textContent = pokeSearchBar.value;
+    pokemonName.textContent = firstUpperCase(pokeSearchBar.value);
     pokePic.src = pokeData.sprites.front_default;
     pokeSearchBar.value = "";
+    console.log(pokeData.abilities)
   })
 
 
   document.addEventListener('keydown', async (e) => {
+
+
     if (e.key === "Enter") {
       console.log("entered")
       const pokeData = await fetchPokemon(pokeSearchBar.value);
       // console.log(pokeData.sprites.front_default)
-      pokemonName.textContent = pokeSearchBar.value;
+      pokemonName.textContent = firstUpperCase(pokeSearchBar.value);
+      console.log(typeof(pokeSearchBar.value))
       pokePic.src = pokeData.sprites.front_default
       pokeSearchBar.value = "";
+      console.log(pokeData.abilities);
+      const pokeAbilities = pokeData.abilities;
+
+      //adds abilities
+      const abilitiesSection = document.querySelector(".ability-section")
+      while (abilitiesSection.firstChild) {
+        abilitiesSection.firstChild.remove()
+      }
+      pokeAbilities.forEach(ability => {
+        const abilityDiv = document.createElement("div");
+        abilityDiv.className = "ability";
+        abilityDiv.textContent = firstUpperCase(ability.ability.name);
+        abilitiesSection.append(abilityDiv);
+      })
+
+      //adds physical attributes
+      const physicalSection = document.querySelector(".physical-section");
+      while (physicalSection.firstChild) {
+        physicalSection.firstChild.remove()
+      }
+      const physicalArray = ["height", "weight", "stats"];
+      for (let i = 0; i < physicalArray.length; i++) {
+        if (physicalArray[i] === "stats") {
+          const physicalStat = pokeData[physicalArray[i]][0].base_stat
+          const physicalDiv = document.createElement("div");
+          physicalDiv.className = "physicalStat";
+          physicalDiv.textContent = `HP: ${physicalStat}`;
+          physicalSection.append(physicalDiv);
+        } else {
+          const physicalStat = pokeData[physicalArray[i]]
+          const physicalDiv = document.createElement("div");
+          physicalDiv.className = "physicalStat";
+          // physicalDiv.textContent = `${physicalArray[i].charAt(0).toUpperCase() + physicalArray[i].slice(1)}: ${physicalStat}`;
+          physicalDiv.textContent = `${firstUpperCase(physicalArray[i])}: ${physicalStat}`;
+          physicalSection.append(physicalDiv);
+        }
+      }
+
+      //adds types
+      const pokeTypes = pokeData.types;
+      const typesSection = document.querySelector(".types-section");
+      while (typesSection.firstChild) {
+        typesSection.firstChild.remove()
+      }
+      pokeTypes.forEach(type => {
+        const typesDiv = document.createElement("div");
+        typesDiv.className = "type";
+        typesDiv.textContent = firstUpperCase(type.type.name);
+        typesSection.append(typesDiv);
+      })
+      pokemonName.classList.remove("hide");
+      imageContainer.classList.remove("hide");
+
+      //unhides information when a pokemon is searched
+      const abilitiesContainer = document.querySelector(".abilities")
+      abilitiesContainer.classList.remove("hide");
+      const typesContainer = document.querySelector(".types")
+      typesContainer.classList.remove("hide");
+      const physicalContainer = document.querySelector(".physicalTraits")
+      physicalContainer.classList.remove("hide");
     }
   })
 }
