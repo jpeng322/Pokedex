12f4eccf5bda61ca8721d6ec2fcfb81a12a359d2
Jacky <jackypengsmail@gmail.com>
2022-11-18 21:00:52 -0500

    Implemented error functionality and updated styling

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
 import axios from "axios"
 
 
-// fetchAllPokemon()
-
 //npm create vite@latest pokedex-web -- --template vanilla
 
 function firstUpperCase(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-// async function fetchAllPokemon() {
-//   try {
-//     const allPokemonData = await axios.get("https://pokeapi.co/api/v2/pokemon/?offset=340&limit=20")
-//     if (allPokemonData.status == 200) {
-//       console.log(allPokemonData)
-//       return allPokemonData.data
-//     } else {
-//       return null
-//     }
-//   } catch (error) {
-//     console.log("something went wrongasdasdsa");
-//     console.log(error)
-//   }
-// }
-
 //fetches one pokemon
 async function fetchPokemon(name) {
   try {
     const pokemonData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
     if (pokemonData.status == 200) {
-      console.log(pokemonData.data)
+      console.log(pokemonData)
       return pokemonData.data
       // return pokemonData.data
     } else {
+      console.log("failed fetch")
       return null
     }
   } catch (error) {
-    console.log(error)
+    // console.log(error)
+    // console.log("hahahah")
+    // console.log(pokemonData, "cannot work")
+    const imageContainer = document.querySelector(".imageContainer")
+    const pokePic = document.createElement("img")
+    const displayContainer = document.querySelector(".displayContainer")
+    displayContainer.classList.remove("default")
+    imageContainer.classList.remove("hide");
+    // imageContainer.append(pokePic);
+    // pokePic.src 
+    // imageContainer.textContent = "NONONO"
+    pokePic.src = "./images/psyduck.jpg"
+    return
   }
 }
+
+
 
 
 async function searchPokemon() {
@@ -55,20 +53,14 @@ async function searchPokemon() {
   const pokePic = document.createElement("img")
   imageContainer.append(pokePic);
 
-  // pokePic.src = fetchPokemon("bulbasaur").sprites.front_default
+
+
   pokeSearchBtn.addEventListener("click", async () => {
     const pokeData = await fetchPokemon(pokeSearchBar.value);
-    pokemonName.textContent = firstUpperCase(pokeSearchBar.value);
-    pokePic.src = pokeData.sprites.front_default;
-    pokeSearchBar.value = "";
-  })
-
-
-  document.addEventListener('keydown', async (e) => {
-
-
-    if (e.key === "Enter") {
-      const pokeData = await fetchPokemon(pokeSearchBar.value);
+    if (pokeData !== undefined) {
+      imageContainer.classList.remove("full")
+      const allButtons = document.querySelectorAll(".btn");
+      allButtons.forEach(button => button.disabled = false)
       pokemonName.textContent = firstUpperCase(pokeSearchBar.value);
       pokePic.src = pokeData.sprites.front_default
       pokeSearchBar.value = "";
@@ -125,6 +117,122 @@ async function searchPokemon() {
       imageContainer.classList.remove("hide");
 
       //unhides information when a pokemon is searched
+      const displayContainer = document.querySelector(".displayContainer")
+      displayContainer.classList.remove("default")
+      const abilitiesContainer = document.querySelector(".abilities")
+      abilitiesContainer.classList.remove("hide");
+      const typesContainer = document.querySelector(".types")
+      typesContainer.classList.remove("hide");
+      const physicalContainer = document.querySelector(".physicalTraits")
+      physicalContainer.classList.remove("hide");
+    }
+    else {
+      const allButtons = document.querySelectorAll(".btn");
+      allButtons.forEach(button => button.disabled = true)
+      console.log(allButtons)
+      console.log("else statement")
+      // const imageContainer = document.querySelector(".imageContainer")
+      // const pokePic = document.createElement("img")
+      const displayContainer = document.querySelector(".displayContainer")
+      displayContainer.classList.remove("default")
+      imageContainer.classList.remove("hide");
+      // imageContainer.append(pokePic);
+      // pokePic.src 
+      // imageContainer.textContent = "NONONO"
+      const typesSection = document.querySelector(".types-section");
+      while (typesSection.firstChild) {
+        typesSection.firstChild.remove()
+      }
+      const physicalSection = document.querySelector(".physical-section");
+      while (physicalSection.firstChild) {
+        physicalSection.firstChild.remove()
+      }
+      const abilitiesSection = document.querySelector(".ability-section")
+      while (abilitiesSection.firstChild) {
+        abilitiesSection.firstChild.remove()
+      }
+      const abilitiesContainer = document.querySelector(".abilities")
+      abilitiesContainer.classList.add("hide");
+      const typesContainer = document.querySelector(".types")
+      typesContainer.classList.add("hide");
+      const physicalContainer = document.querySelector(".physicalTraits")
+      physicalContainer.classList.add("hide");
+      imageContainer.classList.add("full")
+      pokemonName.textContent = ""
+      pokePic.src = "./images/error_page.png"
+      // pokePic.width = "300px"
+
+    }
+  }
+  )
+
+
+
+
+
+  document.addEventListener('keydown', async (e) => {
+
+
+    if (e.key === "Enter") {
+      const pokeData = await fetchPokemon(pokeSearchBar.value);
+      // pokemonName.textContent = firstUpperCase(pokeSearchBar.value);
+      pokePic.src = pokeData.sprites.front_default
+      pokeSearchBar.value = "";
+      const pokeAbilities = pokeData.abilities;
+
+      //adds abilities
+      const abilitiesSection = document.querySelector(".ability-section")
+      while (abilitiesSection.firstChild) {
+        abilitiesSection.firstChild.remove()
+      }
+      pokeAbilities.forEach(ability => {
+        const abilityDiv = document.createElement("div");
+        abilityDiv.className = "ability";
+        abilityDiv.textContent = firstUpperCase(ability.ability.name);
+        abilitiesSection.append(abilityDiv);
+      })
+
+      //adds physical attributes
+      const physicalSection = document.querySelector(".physical-section");
+      while (physicalSection.firstChild) {
+        physicalSection.firstChild.remove()
+      }
+      const physicalArray = ["height", "weight", "stats"];
+      for (let i = 0; i < physicalArray.length; i++) {
+        if (physicalArray[i] === "stats") {
+          const physicalStat = pokeData[physicalArray[i]][0].base_stat
+          const physicalDiv = document.createElement("div");
+          physicalDiv.className = "physicalStat";
+          physicalDiv.textContent = `HP: ${physicalStat}`;
+          physicalSection.append(physicalDiv);
+        } else {
+          const physicalStat = pokeData[physicalArray[i]]
+          const physicalDiv = document.createElement("div");
+          physicalDiv.className = "physicalStat";
+          // physicalDiv.textContent = `${physicalArray[i].charAt(0).toUpperCase() + physicalArray[i].slice(1)}: ${physicalStat}`;
+          physicalDiv.textContent = `${firstUpperCase(physicalArray[i])}: ${physicalStat}`;
+          physicalSection.append(physicalDiv);
+        }
+      }
+
+      //adds types
+      const pokeTypes = pokeData.types;
+      const typesSection = document.querySelector(".types-section");
+      while (typesSection.firstChild) {
+        typesSection.firstChild.remove()
+      }
+      pokeTypes.forEach(type => {
+        const typesDiv = document.createElement("div");
+        typesDiv.className = "type";
+        typesDiv.textContent = firstUpperCase(type.type.name);
+        typesSection.append(typesDiv);
+      })
+      pokemonName.classList.remove("hide");
+      imageContainer.classList.remove("hide");
+
+      //unhides information when a pokemon is searched
+      const displayContainer = document.querySelector(".displayContainer")
+      displayContainer.classList.remove("default")
       const abilitiesContainer = document.querySelector(".abilities")
       abilitiesContainer.classList.remove("hide");
       const typesContainer = document.querySelector(".types")
@@ -147,10 +255,10 @@ async function makeOtherSpritesArray() {
   console.log(pokeData.sprites.other)
   const otherSprites = pokeData.sprites.other
   pokeImg.src = otherSprites[otherSpritesArray[counter2]].front_default
-  }
+}
 
 let counter = 0;
-let counter2 =0;
+let counter2 = 0;
 
 async function changeImg() {
   const pokeData = await fetchPokemon(pokemonName.textContent.toLowerCase())
