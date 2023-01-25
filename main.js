25b458d14f6558a229a89d6461e2e5309a3fd6c6
Jacky <jackypengsmail@gmail.com>
2023-01-25 15:55:16 -0500

    Testing different error image route

--- main.js
+++ main.js
@@ -137,7 +137,7 @@ async function displayPokemon() {
       physicalContainer.classList.add("hide");
       imageContainer.classList.add("full")
       pokemonName.textContent = ""
-      pokePic.src = "./images/error_page.png"
+      pokePic.src = "../images/error_page.png"
       // pokePic.width = "300px"
 
     }
